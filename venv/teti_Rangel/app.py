4f35a818d7649f2a39cb0996589755809756b3f5
rangel3l <rangel.silva@estudante.ifms.edu.br>
2022-08-17 23:42:15 -0400

    meu commit

---
+++ venv/teti_Rangel/app.py
@@ -0,0 +1,35 @@
+from crypt import methods
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello_world():
+
+    return "<p>unidade curricular!</p>"
+
+@app.route('/aula17')  
+def aula17():
+     return render_template('aula17-08.html') 
+
+@app.route('/lerForm', methods = ['POST'])
+def lerForm():
+    if request.method =='POST':
+       nome = request.form.get('nome')
+       return render_template('aula17-08.html', nome = nome)  
+
+@app.route('/lista')
+def lista():
+    listaNomes = ['pedro', 'kleber', 'vanessa']
+    return render_template('lista.html', listaNomes = listaNomes)
+    
+@app.route('/unidadeCurricular')       
+def unidadeCurricular():
+    numero = 30
+    return render_template('unidadeCurricular.html')
+        
+if __name__ == "__main__" :    
+    app.run(debug= True)
+
+#flash run --host= 0.0.0.0.
